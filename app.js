41c905f83170b7957dda3235ca644cd5874bd2df
Sing Khan Tuang <singkhantuang79@gmail.com>
2023-03-14 13:29:24 +0630

    modified

--- app.js
+++ app.js
@@ -5,44 +5,86 @@ const fs = require("fs");
 const moment = require("moment");
 const path = require("path");
 
-const baseUrl = "https://dev.antares.chat";
-const maxPages = 60;
+const category = "health-beauty";
+const baseUrl = `https://myanmarload.com`;
+const maxPages = 20;
 let pagesCrawled = 0;
 let pagesToCrawl = [baseUrl];
+// for (let i = 2; i <= maxPages; i++) {
+//   pagesToCrawl.push(`${baseUrl}/category/${category}?page=${i}#latestArticle`);
+// }
+
 const content = {};
 
 async function crawlPage(pageUrl) {
   try {
-    const response = await axios.get(pageUrl);
+    const response = await axios.get(`${pageUrl}`);
     const html = response.data;
     const $ = cheerio.load(html);
 
     const pageTitle = $("title").text();
     const pageDescription = $('meta[name="description"]').attr("content") || "";
-    const pageHeaders = [];
-    $("h1,h2,h3,h4,h5,h6").each(function () {
-      pageHeaders.push($(this).text());
+    // const pageHeaders = [];
+    // $("h1,h2,h3,h4,h5,h6").each(function () {
+    //   pageHeaders.push($(this).text());
+    // });
+    // const pageParagraph = [];
+    const pageArticle = [];
+    // $("p").each(function () {
+    //   pageParagraph.push($(this).text());
+    // });
+    // $("article").each(function () {
+    //   const $article = $(this);
+    //   const $h3 = $article.find('h1,h2,h3');
+    //   const $paragraphs = $article.find('p');
+    //   const title = $h3.text();
+    //   const text = $paragraphs.text();
+    //   pageArticle[title] = text;
+    // });
+
+    $("article").each(function () {
+      const $h3 = $(this).find("h1,h2,h3");
+      const $paragraphs = $(this).find("p");
+      const $images = $(this).find("img");
+      const title = $h3.text();
+      const paragraphs = [];
+      const images = [];
+
+      $paragraphs.each(function () {
+        paragraphs.push($(this).text());
+      });
+
+      $images.each(function () {
+        images.push($(this).attr("src"));
+      });
+
+      pageArticle.push({ [title]: { paragraphs, images } });
     });
-    const pageParagraph = [];
-    $("p").each(function () {
-      pageParagraph.push($(this).text());
-    });
-    const pageImages = [];
-    $("img").each(function () {
-      const imageUrl = $(this).attr("src");
-      if (imageUrl) {
-        const absoluteUrl = url.resolve(baseUrl, imageUrl);
-        pageImages.push(absoluteUrl);
-      }
-    });
+
+    // pageArticle.forEach(function (article) {
+    //   for (const [title, paragraphs] of Object.entries(article)) {
+    //     console.log(title);
+    //     console.log(paragraphs);
+    //   }
+    // });
+
+    // const pageImages = [];
+    // $("img").each(function () {
+    //   const imageUrl = $(this).attr("src");
+    //   if (imageUrl) {
+    //     const absoluteUrl = url.resolve(baseUrl, imageUrl);
+    //     pageImages.push(absoluteUrl);
+    //   }
+    // });
 
     content["crawled_links"] = null;
     content[pageUrl] = {
       title: pageTitle,
       description: pageDescription,
-      headers: pageHeaders,
-      paragraph: pageParagraph,
-      images: pageImages,
+      // headers: pageHeaders,
+      // paragraph: pageParagraph,
+      article: pageArticle,
+      // images: pageImages,
     };
 
     $("a").each(function () {
